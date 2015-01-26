8764cfbe95a4509c917e89138c0e5c59ca45bf19
Ziwei Xie <dz.cies@zoho.com>
2015-01-26 22:23:25 +0800

    Solution for 15_refs, second half.

--- src/koans/15_refs.clj
+++ src/koans/15_refs.clj
@@ -25,12 +25,12 @@
           @the-world))
 
   "Don't forget to do your work in a transaction!"
-  (= 0 (do __
+  (= 0 (do (dosync (ref-set the-world 0))
            @the-world))
 
   "Functions passed to alter may depend on the data in the ref"
   (= 20 (do
-          (dosync (alter the-world ___))))
+          (dosync (alter the-world #(+ % 20)))))
 
   "Two worlds are better than one"
   (= ["Real Jerry" "Bizarro Jerry"]
@@ -39,4 +39,4 @@
           (ref-set the-world {})
           (alter the-world assoc :jerry "Real Jerry")
           (alter bizarro-world assoc :jerry "Bizarro Jerry")
-          __))))
+          (map :jerry [@the-world @bizarro-world])))))
